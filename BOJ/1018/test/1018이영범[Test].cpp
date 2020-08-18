28b39a0bb932732e9acfae7613739bdb703bb7e6
Dev-Beom <66074802+Dev-Beom@users.noreply.github.com>
2020-08-18 17:15:40 +0900

    1018

--- BOJ/1018/test/1018이영범[Test].cpp
+++ BOJ/1018/test/1018이영범[Test].cpp
@@ -4,16 +4,40 @@ int main(int argc, char const *argv[])
 {
     int row = 0;
     int column = 0;
-    int minim = 0;
+    int minim = 9999;
+
     int cNum = 0;
 
-    std::cout << "start2" << std::endl;
+    int bNum = 0;
+    int wNum = 0;
+
+    int bMinim = 9999;
+    int wMinim = 9999;
+
+    char bMap[8][8] = {
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}};
+
+    char wMap[8][8] = {
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}};
+
+    // std::cout << "start2" << std::endl;
 
     std::cin >> row;
     std::cin >> column;
-
-    int sizeN = row - 8;
-    int sizeM = column - 8;
 
     char **ary = new char *[row];
     for (int i = 0; i < row; i++)
@@ -28,54 +52,56 @@ int main(int argc, char const *argv[])
             std::cin >> ary[i][j];
         }
     }
-    for (int i = 0; i < row; i++)
-    { //확인위해 값 출력
-        for (int j = 0; j < column; j++)
-        {
-            std::cout << ary[i][j] << " ";
-        }
-        std::cout << '\n';
-    }
-    for (int n = 0; n <= sizeN; n++)
+    // for (int i = 0; i < row; i++)
+    // { //확인위해 값 출력
+    //     for (int j = 0; j < column; j++)
+    //     {
+    //         std::cout << ary[i][j] << " ";
+    //     }
+    //     std::cout << '\n';
+    // }
+
+    for (int n = 0; n <= row - 8; n++)
     {
-        std::cout << "Start SizeN Loop" << std::endl;
-        for (int m = 0; m <= sizeM; m++)
+        for (int m = 0; m <= column - 8; m++)
         {
-            std::cout << "Start SizeM Loop" << std::endl;
-            for (int i = 0; i < row; i++)
+            for (int i = 0; i < 8; i++)
             {
-                for (int j = 0; j < column; j++)
+                for (int j = 0; j < 8; j++)
                 {
-                    
+                    if (ary[i + n][j + m] != bMap[i][j])
+                    {
+                        bNum++;
+                    }
+                    if (ary[i + n][j + m] != wMap[i][j])
+                    {
+                        wNum++;
+                    }
                 }
             }
-            if (cNum <= minim)
+            if (bNum <= bMinim)
             {
-                minim = cNum;
-                cNum = 0;
+                bMinim = bNum;
             }
-            else
+            if (wNum <= wMinim)
             {
-                cNum = 0;
+                wMinim = wNum;
             }
+            bNum = 0;
+            wNum = 0;
         }
     }
-
-    for (int i = 0; i < row; i++)
+    if (bMinim <= wMinim)
     {
-        for (int j = 0; j < column; j++)
-        {
-            if (ary[i][j] == ary[i][j + 1])
-            {
-                // if(ary[i][j] == ary[i + 1][j]){
-                // count ++;
-
-                // }
-            }
-        }
+        minim = bMinim;
     }
-    std::cout << "count : ";
-    std::cout << cNum << std::endl;
+    else
+    {
+        minim = wMinim;
+    }
+
+    // std::cout << "count : ";
+    std::cout << minim << std::endl;
     for (int i = 0; i < row; i++)
     { //메모리 해제 , 행마다 해제해주고
         delete[] ary[i];
